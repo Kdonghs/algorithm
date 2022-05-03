d5fa675f871bbe230a7e49b9b0d03b06e51551b2
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2022-05-03 17:07:28 +0900

    문제풀이 추가

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 import sys
-from collections import deque
 
-T = int(sys.stdin.readline().strip())
+#점화식 li[n] = li[n-1] + li[n-2] + li[n-3]
+t = int(sys.stdin.readline().strip())
+li = []
+result = [1,2,4]
 
-for i in range(T):
-    p = [i for i in sys.stdin.readline().strip()]
-    n = int(sys.stdin.readline().strip())
-    queue = deque(sys.stdin.readline().strip()[1:-1].split(','))
+#입력받기
+for i in range(t):
+    li.append(int(sys.stdin.readline().strip()))
 
-    rev= True
-    flag = True
-    if n == 0:
-        queue = []
+#입력값중 최대만큼 경우의 수 계산
+for i in range(max(li)):
+    result.append(result[i] + result[i+1] + result[i+2])
 
-
-    for j in p:
-        if j == 'R':
-            rev = not rev
-        elif j == 'D':
-            if len(queue) < 1:
-                flag = not flag
-                print("error")
-                break
-            else:
-                if rev:
-                    queue.popleft()
-                else:
-                    queue.pop()
-    if flag:
-        if rev:
-            print("[" + ",".join(queue) + "]")
-        else:
-            queue.reverse()
-            print("[" + ",".join(queue) + "]")
+#해당하는 값의 경우의수 출력
+for i in li:
+    print(result[i-1])
