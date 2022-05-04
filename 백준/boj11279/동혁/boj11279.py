7bbf4af1ef1decae46035f0d8fe7fdc6653ca942
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2022-05-04 20:51:31 +0900

    문제풀이 추가

--- 백준/boj11279/동혁/boj11279.py
+++ 백준/boj11279/동혁/boj11279.py
@@ -4,6 +4,8 @@ import heapq
 n=int(sys.stdin.readline().strip())
 li=[]
 
+#heapq를 사용 (최소 힙을 구현하는 모듈)
+#-a 형태로 넣어서 보관하고 꺼낼때 다시 -를 해줘서 최대 큐로 사용
 for i in range(n):
     a=int(sys.stdin.readline().strip())
 
