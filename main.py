dcb647fdeef337f0f88bb3bd835871aaa342ee43
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2022-05-10 01:33:24 +0900

    문제풀이 추가

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-#글자를 숫자로 치환
-answer =s
-answer = answer.replace('zero','0')
-answer = answer.replace('one','1')
-answer = answer.replace('two','2')
-answer = answer.replace('three','3')
-answer = answer.replace('four','4')
-answer = answer.replace('five','5')
-answer = answer.replace('six','6')
-answer = answer.replace('seven','7')
-answer = answer.replace('eight','8')
-answer = answer.replace('nine','9')
+numbers = [1,2,3,4,6,7,8,0]
+li = [i for i in range(10)]
 
+for i in numbers:
+    li.remove(i)
+print(sum(li))
