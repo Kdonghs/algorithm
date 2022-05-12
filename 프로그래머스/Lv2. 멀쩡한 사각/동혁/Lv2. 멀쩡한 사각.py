600be9e9e6e56f6047aa48515ea90ecd7d2c9779
김동혁 <kkk951744@naver.com>
2022-05-12 15:48:43 +0900

    문제풀이 추가

--- 프로그래머스/Lv2. 멀쩡한 사각/동혁/Lv2. 멀쩡한 사각.py
+++ 프로그래머스/Lv2. 멀쩡한 사각/동혁/Lv2. 멀쩡한 사각.py
@@ -1,4 +1,7 @@
 def solution(w,h):
+    # x * y - ( x + y - 최대 공약수)
+    #ex) (8,12) 96  - (8 + 12 - 4) = 18
+    #ex) (5,5) 25 - (5 + 5 -5) = 20
     from math import gcd
     answer = w*h
     g = gcd(w,h)
